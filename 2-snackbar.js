0d090756dca76caa765425a992ec883cafd536bf
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-09-23 20:17:37 +0000

    Deploying to gh-pages from @ Kristina-16-Kaguy/goit-js-hw-10@2a37b434472ba32c06f67c95c911b82ba84e978c 🚀

--- 2-snackbar.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i}from"./assets/vendor-BbbuE1sJ.js";i.show({title:"привіт",message:"заміни мене на windows.alert"});
-//# sourceMappingURL=2-snackbar.js.map
