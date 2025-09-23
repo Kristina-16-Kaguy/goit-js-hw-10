f6fe1df554bd6d0810b454a5221af2692b796ebe
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-09-23 20:57:19 +0300

    final comit 1 task

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,10 +6,14 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const inputEl = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('[data-start]');
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEL = document.querySelector('[data-seconds]');
+
+startButton.disabled = true;
+let userSelectedDate = null;
+let intervalId;
 
 const options = {
   enableTime: true,
@@ -17,14 +21,56 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0].getTime();
+
+    const currentTime = Date.now();
+
+    if (userSelectedDate <= currentTime) {
+      // window.alert('Please choose a date in the future');
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
   },
 };
 
 flatpickr(inputEl, options);
 
-let userSelectedDate = null;
-let timerId = null;
+function updateTimer() {
+  let remainingTime = userSelectedDate - Date.now();
+
+  if (remainingTime <= 0) {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+    startButton.disabled = false;
+    inputEl.disabled = false;
+    setDateOnUi({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  } else {
+    const dateObj = convertMs(remainingTime);
+    setDateOnUi(dateObj);
+  }
+}
+
+function setDateOnUi({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days.toString().padStart(2, '0');
+  hoursEl.textContent = hours.toString().padStart(2, '0');
+  minutesEl.textContent = minutes.toString().padStart(2, '0');
+  secondsEL.textContent = seconds.toString().padStart(2, '0');
+}
+
+startButton.addEventListener('click', e => {
+  startButton.disabled = true;
+  inputEl.disabled = true;
+  updateTimer();
+  intervalId = setInterval(updateTimer, 1000);
+});
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -49,8 +95,7 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-iziToast.show({
-  title: 'Привіт',
-  message: 'заміни мене на windows.alert',
-});
-// вставити  замість windows.alert
+// iziToast.show({
+//   title: 'Привіт',
+//   message: 'заміни мене на windows.alert',
+// });
