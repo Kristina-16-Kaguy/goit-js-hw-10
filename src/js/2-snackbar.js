2a37b434472ba32c06f67c95c911b82ba84e978c
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-09-23 23:17:14 +0300

    second task css

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-iziToast.show({
-  title: 'привіт',
-  message: 'заміни мене на windows.alert',
-});
+// iziToast.show({
+//   title: 'привіт',
+//   message: 'заміни мене на windows.alert',
+// });
