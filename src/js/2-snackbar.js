4128434958a4950d20a5c714bde1e4f974c71f70
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-09-25 01:09:28 +0300

    final commit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,29 +5,35 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+  const delay = event.target.elements.delay.value;
 
-  const delay = Number(form.elements.delay.value);
-  const state = form.elements.state.value;
-
-  const promise = new Promise((resolve, reject) => {
+  if (delay < 0) {
+    iziToast.error({
+      title: 'Error',
+      message: `invalid ${delay} value`,
+    });
+  }
+  const radioBtn = event.target.elements.state.value;
+  event.target.reset();
+  new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (state === 'fulfilled') {
+      if (radioBtn === 'fulfilled') {
         resolve(delay);
       } else {
         reject(delay);
       }
-    }, delay);
-  });
-
-  promise
+    });
+  }, delay)
     .then(delay => {
       iziToast.success({
+        title: 'OK',
         message: `✅ Fulfilled promise in ${delay}ms`,
         position: 'topRight',
       });
     })
     .catch(delay => {
       iziToast.error({
+        title: 'Error',
         message: `❌ Rejected promise in ${delay}ms`,
         position: 'topRight',
       });
